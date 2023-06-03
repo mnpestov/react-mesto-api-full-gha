6acd0a5a6d36ba666a437ef8f2e93ca79aaabc01
Mikhail Pestov <mnpestov@gmail.com>
2023-06-03 21:04:15 +0300

    mongo

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.use(errors());
 app.use(errorHandler);
 
 async function connect() {
-  await mongoose.connect('mongodb://localhost:27017/mestodb ', {
+  await mongoose.connect('mongodb://127.0.0.1:27017/mestodb ', {
     useNewUrlParser: true,
   });
   await app.listen(PORT);

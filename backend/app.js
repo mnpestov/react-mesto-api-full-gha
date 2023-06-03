38ee4df240279fa4ae1319bea45eea5fb9d2ca76
Mikhail Pestov <mnpestov@gmail.com>
2023-06-04 00:01:32 +0300

    добавил пакет cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 // const httpConstants = require('http2').constants;
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { celebrate, Joi, errors } = require('celebrate');
 const { routes } = require('./routes');
 const {
@@ -11,10 +12,10 @@ const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
-const { PORT = 3000 } = process.env;
-
+app.use(cors());
 app.use(requestLogger);
 app.post('/signin', express.json(), celebrate({
   body: Joi.object().keys({

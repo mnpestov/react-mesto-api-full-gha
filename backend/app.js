002a4fc69776830fce01f7e73f9e88c775a6eafc
Mikhail Pestov <mnpestov@gmail.com>
2023-06-07 16:53:09 +0300

    Простые запросы cors

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,23 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env.PORT;
 const app = express();
 
+const allowedCors = [
+  'https://mnpestov.nomoredomains.rocks',
+  'http://mnpestov.nomoredomains.rocks',
+];
+
 app.use(cors());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
 app.use(requestLogger);
 
 // Код для проведения краш-теста
